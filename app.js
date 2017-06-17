45707b69919da61e4ed96184f6e5c6969e1c15b0
Jason <atxjdc@gmail.com>
2017-06-17 01:27:34 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,94 @@
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+    
+mongoose.connect("mongodb://olak:041427414000@ds129442.mlab.com:29442/tech_solutions");
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "the cake is still a lie",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static("public"));
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//============
+// ROUTES
+//============
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/main",isLoggedIn, function(req, res){
+   res.render("main"); 
+});
+
+// Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handling user sign up
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/main");
+        });
+    });
+});
+
+// LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/main",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+//Category Sub-Pages
+
+app.get("/cookies",isLoggedIn, function(req, res){
+   res.render("cookies"); 
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The server is running so you had better catch it!");
+})
